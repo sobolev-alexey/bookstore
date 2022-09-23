8e44138499da291a1d894c2e58fb2c9d53c7ea3c
Alexey Sobolev <sobolev@mail.de>
2022-09-23 18:55:00 +0200

    Added routing, moved landing page content to the respective page

---
+++ src/pages/Landing.js
@@ -0,0 +1,107 @@
+import React, { useContext } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import { AppContext } from '../context/globalState';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
+}));
+
+const Landing = () => {
+  const { books } = useContext(AppContext);
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Book Shop
+          </Typography>
+          <Button color="inherit">Cart</Button>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.content}>
+        <BookCard books={books} />
+      </main>
+    </div>
+  );
+}
+
+const cardStyles = makeStyles({
+  root: {
+    minWidth: '50%',
+    marginBottom: 8,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+function BookCard(props) {
+  const classes = cardStyles();
+
+  const { books } = props;
+
+  return (
+    <>
+      {Object.keys(books)?.map((bookIndex, index) => {
+        const book = books[bookIndex];
+
+        return (
+          <Card key={`${book?.Title}-${index}`} className={classes.root}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                {book?.Title}
+              </Typography>
+              <Typography
+                className={classes.title}
+                color="textSecondary"
+                gutterBottom
+              >
+                {book?.Author}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Book in detail</Button>
+            </CardActions>
+          </Card>
+        );
+      })}
+    </>
+  );
+}
+
+export default Landing;
