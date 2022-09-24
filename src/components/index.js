f0ec1c71cbed9495c6285a7613205999b4ee8fa8
Alexey Sobolev <sobolev@mail.de>
2022-09-25 01:38:51 +0200

    Exported components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,21 @@
 import BookCard from './BookCard';
+import Header from './Header';
+import Footer from './Footer';
+import Banner from './Banner';
+import BookCarousel from './BookCarousel';
+import Cart from './Cart';
+import Layout from './Layout';
+import Search from './Search';
+import ShippingBanner from './ShippingBanner';
 
-export { BookCard };
+export { 
+  BookCard,
+  Header,
+  Footer,
+  Banner,
+  BookCarousel,
+  Cart,
+  Layout,
+  Search,
+  ShippingBanner,
+};
