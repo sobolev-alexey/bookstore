be173dc1465736f7d1442e8d3887c2583b7985b7
Alexey Sobolev <sobolev@mail.de>
2022-09-25 01:37:06 +0200

    Banner component with carousel effect

---
+++ src/components/Banner.js
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Carousel } from 'antd';
+import { AppContext } from '../context/globalState';
+import discountBanner from '../assets/discount.jpg';
+
+const Banner = () => {
+  const { books } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  return (
+    <div className="banner-carousel-wrapper">
+      <Carousel 
+        autoplay
+        autoplaySpeed={7000}
+        dots={false}
+      >
+        <div>
+          <img src={discountBanner} alt="10% Discount" className="carousel-item discount-banner"/>
+        </div>
+        {
+          books
+            ?.sort((a,b) => a?.AverageRating - b?.AverageRating)
+            ?.slice(0, 3)
+            ?.map(book => (
+              <div key={book?.Title}>
+                <div className="banner-book-cover carousel-item" key={book?.Title}>
+                  <img src={book?.Cover} alt={book?.Title} className="banner-book-cover" />
+                  <div className="book-details">
+                    <h3>{book?.Title}</h3>
+                    <p>by {book?.Author}</p>
+                    <button className="primary" onClick={() => navigate(`/book/${book.ID}`)}>Details</button>
+                  </div>
+                </div>
+              </div>
+            ))
+        }
+      </Carousel>
+    </div>
+  );
+};
+
+export default Banner;
