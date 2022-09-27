3fceb467408aebe950ffdc7e6f795b89d6267a5c
Alexey Sobolev <sobolev@mail.de>
2022-09-27 20:01:45 +0200

    Checkout address form

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
+import { Form, Input } from 'antd';
 import { Layout } from '../components';
 import { getPriceLabel } from '../utils/helpers';
 import { AppContext } from '../context/globalState';
@@ -6,6 +7,7 @@ import cartIcon from '../assets/header/basket-shopping-solid.svg';
 
 const Checkout = () => {
   const { basket } = useContext(AppContext);
+  const [form] = Form.useForm();
   const [total, setTotal] = useState(0);
   const [isOrderPlaced, setOrderPlaced] = useState(false);
 
@@ -19,6 +21,10 @@ const Checkout = () => {
     );
   }, [basket?.total]);
 
+  const processPayment = async () => {
+
+  }
+
   return (
     <Layout>
     <div className="checkout-page-wrapper">
@@ -29,10 +35,78 @@ const Checkout = () => {
           <div className={`step ${isOrderPlaced && 'active'}`}>2. Order placed</div>
         </div>
         <div className="card address-details-wrapper">
-          Address
+          <h3>1. Delivery Address</h3>
+          <Form
+            layout='vertical'
+            form={form}
+            validateTrigger='onChange'
+            scrollToFirstError
+          >
+            <Form.Item 
+              required 
+              // hasFeedback
+              name="name"
+              label="Full name"
+              extra="First and last name. For example: John Smith"
+              rules={[{ required: true, message: 'Please enter your Full name' }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item 
+              required 
+              // hasFeedback
+              name="address1"
+              label="Address line 1"
+              extra="For example: street address, PO box, company name, c/o"
+              rules={[{ required: true, message: 'Please enter your Address line 1' }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item 
+              name="address2"
+              label="Address line 2"
+              extra="For example: apartment, suite, unit, building, floor"
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item 
+              required 
+              // hasFeedback
+              name="city"
+              label="Town/City"
+              rules={[{ required: true, message: 'Please enter your Town/City' }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item 
+              name="state"
+              label="County/State"
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item 
+              required 
+              // hasFeedback
+              name="postcode"
+              label="Postcode/ZIP"
+              extra="If you don't have a postcode or ZIP, please write 'No Postcode'"
+              rules={[{ required: true, message: "Please enter your postcode/ZIP or write 'No Postcode'" }]}
+            >
+              <Input />
+            </Form.Item>
+          </Form>
         </div>
         <div className="card payment-details-wrapper">
-          Payment
+          <h3>2. Payment</h3>
+          <button className="primary" onClick={processPayment}>Buy now</button>
+          <p className="consent">
+            In placing your order, you are confirming that you have read and agree to our <u>Terms and Conditions</u>. Please also see our <u>Privacy Policy</u> and our <u>Cookies Policy</u>.
+          </p>
         </div>
       </div>
       <div className="checkout-summary-column-wrapper">
