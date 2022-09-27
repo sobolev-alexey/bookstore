e923de45bc048b3dd897066defe85d1bcce80f82
Alexey Sobolev <sobolev@mail.de>
2022-09-28 01:12:00 +0200

    Update checkout page, add order confirmation

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import { Form, Input } from 'antd';
 import {loadStripe} from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
+import { Link, useNavigate } from 'react-router-dom';
 import { Layout, CheckoutPaymentForm } from '../components';
 import { getPriceLabel } from '../utils/helpers';
 import { AppContext } from '../context/globalState';
@@ -12,8 +13,9 @@ const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 const Checkout = () => {
   const { basket } = useContext(AppContext);
   const [form] = Form.useForm();
+  const navigate = useNavigate();
   const [total, setTotal] = useState(0);
-  const [isOrderPlaced, setOrderPlaced] = useState(false);
+  const [orderDetails, setOrderDetails] = useState(null);
 
   useEffect(() => {
     setTotal(
@@ -23,139 +25,169 @@ const Checkout = () => {
         basket.items?.[0]?.ListPrice?.currencyCode
       )
     );
+
+    if (basket?.total === 0) {
+      navigate('/');
+    }
   }, [basket?.total]);
 
-  const processPayment = async (payment) => {
+  const processPayment = async (paymentDetails) => {
     const values = form.getFieldsValue();
-    console.log(values, payment);
+    console.log(values, paymentDetails);
+    setOrderDetails(paymentDetails);
   }
 
   return (
     <Layout>
-    <div className="checkout-page-wrapper">
-      <div className="checkout-main-column-wrapper">
-        <div className="steps-wrapper">
-          <div className="step active">1. Payment details</div>
-          { !isOrderPlaced && <div className="divider" /> }
-          <div className={`step ${isOrderPlaced && 'active'}`}>2. Order placed</div>
-        </div>
-        <div className="card address-details-wrapper">
-          <h3>1. Delivery Address</h3>
-          <Form
-            layout='vertical'
-            form={form}
-            validateTrigger='onChange'
-            scrollToFirstError
-            autoComplete="off"
-            size="large"
-            className="address-form"
-          >
-            <Form.Item 
-              name="name"
-              label="Full name"
-              extra="First and last name. For example: John Smith"
-              rules={[{ required: true, message: 'Please enter your Full name' }]}
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item 
-              name="address1"
-              label="Address line 1"
-              extra="For example: street address, PO box, company name, c/o"
-              rules={[{ required: true, message: 'Please enter your Address line 1' }]}
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item 
-              name="address2"
-              label="Address line 2"
-              extra="For example: apartment, suite, unit, building, floor"
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item 
-              name="city"
-              label="Town/City"
-              rules={[{ required: true, message: 'Please enter your Town/City' }]}
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item 
-              name="state"
-              label="County/State"
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item 
-              name="postcode"
-              label="Postcode/ZIP"
-              extra="If you don't have a postcode or ZIP, please write 'No Postcode'"
-              rules={[{ required: true, message: "Please enter your postcode/ZIP or write 'No Postcode'" }]}
-            >
-              <Input />
-            </Form.Item>
-          </Form>
-        </div>
-        <div className="card payment-details-wrapper">
-          <h3>2. Payment</h3>
-          <Elements stripe={stripePromise}>
-            <CheckoutPaymentForm callback={processPayment} />
-          </Elements>
-          <p className="consent">
-            In placing your order, you are confirming that you have read and agree to our <u>Terms and Conditions</u>. Please also see our <u>Privacy Policy</u> and our <u>Cookies Policy</u>.
-          </p>
-        </div>
-      </div>
-      <div className="checkout-summary-column-wrapper">
-        <div className="summary-details-wrapper">
-          <div className="summary-header">
-            <h3>Order summary</h3>
-            <div className="mini-total-wrapper">
-              <span className="cart-total"><img src={cartIcon} alt="cart" className="cart-icon" /> {basket?.count} items</span>
-              <span className="total">{total}</span>
-            </div>
+      <div className="checkout-page-wrapper">
+        <div className="checkout-main-column-wrapper">
+          <div className="steps-wrapper">
+            <div className="step active">1. Payment details</div>
+            { !orderDetails && <div className="divider" /> }
+            <div className={`step ${orderDetails && 'active'}`}>2. Order placed</div>
           </div>
-          <div className="summary-body">
-            {
-              basket.items?.map(book => (
-                <div className="summary-item">
-                  <div className="title">
-                    <p>{book?.Title} (Paperback)</p>
-                    <p>x{book?.count}</p>
-                  </div>
-                  <p className="price">
-                    { getPriceLabel(book?.ListPrice?.amount, book?.Country, book?.ListPrice?.currencyCode) }
+          {
+            orderDetails ? (
+              <div className="card order-wrapper">
+                <div className="order-title" role="alert">
+                  Thanks for purchasing!
+                </div>
+                <div className="order-message">
+                  Your Order ID is <b>{orderDetails?.orderId}</b>
+                </div>
+                <Link to="/" className="order-cta">
+                  <button className="primary large">
+                    Continue shopping
+                  </button>
+                </Link>
+              </div>
+            ) : (
+              <>
+                <div className="card address-details-wrapper">
+                  <h3>1. Delivery Address</h3>
+                  <Form
+                    layout='vertical'
+                    form={form}
+                    validateTrigger='onChange'
+                    scrollToFirstError
+                    autoComplete="off"
+                    size="large"
+                    className="address-form"
+                  >
+                    <Form.Item 
+                      name="name"
+                      label="Full name"
+                      extra="First and last name. For example: John Smith"
+                      rules={[{ required: true, message: 'Please enter your Full name' }]}
+                    >
+                      <Input />
+                    </Form.Item>
+
+                    <Form.Item 
+                      name="address1"
+                      label="Address line 1"
+                      extra="For example: street address, PO box, company name, c/o"
+                      rules={[{ required: true, message: 'Please enter your Address line 1' }]}
+                    >
+                      <Input />
+                    </Form.Item>
+
+                    <Form.Item 
+                      name="address2"
+                      label="Address line 2"
+                      extra="For example: apartment, suite, unit, building, floor"
+                    >
+                      <Input />
+                    </Form.Item>
+
+                    <Form.Item 
+                      name="city"
+                      label="Town/City"
+                      rules={[{ required: true, message: 'Please enter your Town/City' }]}
+                    >
+                      <Input />
+                    </Form.Item>
+
+                    <Form.Item 
+                      name="state"
+                      label="County/State"
+                    >
+                      <Input />
+                    </Form.Item>
+
+                    <Form.Item 
+                      name="postcode"
+                      label="Postcode/ZIP"
+                      extra="If you don't have a postcode or ZIP, please write 'No Postcode'"
+                      rules={[{ required: true, message: "Please enter your postcode/ZIP or write 'No Postcode'" }]}
+                    >
+                      <Input />
+                    </Form.Item>
+                  </Form>
+                </div>
+                <div className="card payment-details-wrapper">
+                  <h3>2. Payment</h3>
+                  <Elements stripe={stripePromise}>
+                    <CheckoutPaymentForm 
+                      amount={basket?.total} 
+                      currency={basket.items?.[0]?.ListPrice?.currencyCode} 
+                      callback={processPayment} 
+                    />
+                  </Elements>
+                  <p className="consent">
+                    In placing your order, you are confirming that you have read and agree to our <u>Terms and Conditions</u>. Please also see our <u>Privacy Policy</u> and our <u>Cookies Policy</u>.
                   </p>
                 </div>
-              ))
-            }
-            <div className="summary-item">
-              <div className="title">
-                <p className="bold">Sub-total (incl. taxes)</p>
+              </>
+            )
+          }
+
+        </div>
+        <div className="checkout-summary-column-wrapper">
+          <div className="summary-details-wrapper">
+            <div className="summary-header">
+              <h3>Order summary</h3>
+              <div className="mini-total-wrapper">
+                <span className="cart-total"><img src={cartIcon} alt="cart" className="cart-icon" /> {basket?.count} items</span>
+                <span className="total">{total}</span>
               </div>
-              <p className="price">{ total }</p>
             </div>
-            <div className="summary-item">
-              <div className="title">
-                <p className="bold">Delivery</p>
+            <div className="summary-body">
+              {
+                basket.items?.map(book => (
+                  <div className="summary-item" key={book?.ID}>
+                    <div className="title">
+                      <p>{book?.Title} (Paperback)</p>
+                      <p>x{book?.count}</p>
+                    </div>
+                    <p className="price">
+                      { getPriceLabel(book?.ListPrice?.amount, book?.Country, book?.ListPrice?.currencyCode) }
+                    </p>
+                  </div>
+                ))
+              }
+              <div className="summary-item">
+                <div className="title">
+                  <p className="bold">Sub-total (incl. taxes)</p>
+                </div>
+                <p className="price">{ total }</p>
               </div>
-              <p className="price bold">FREE</p>
-            </div>
-            <div className="summary-item">
-              <div className="title">
-                <p className="bold">Total</p>
+              <div className="summary-item">
+                <div className="title">
+                  <p className="bold">Delivery</p>
+                </div>
+                <p className="price bold">FREE</p>
               </div>
-              <p className="price bold highlight">{ total }</p>
+              <div className="summary-item">
+                <div className="title">
+                  <p className="bold">Total</p>
+                </div>
+                <p className="price bold highlight">{ total }</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </Layout>
   );
 };
