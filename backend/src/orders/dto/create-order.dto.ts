3010f74bd0ba4ec37ab0c92a9efbf0294634cbd0
Alexey Sobolev <sobolev@mail.de>
2022-09-28 19:24:17 +0200

    Update order DB schema

--- backend/src/orders/dto/create-order.dto.ts
+++ backend/src/orders/dto/create-order.dto.ts
@@ -1,3 +1,13 @@
 export class CreateOrderDto {
+  name: string;
+  address1: string;
+  address2?: string;
+  city: string;
+  state?: string;
+  postcode: string;
+  orderId: string;
   email: string;
+  cardholder: string;
+  bookIds: string
+  amount: number;
 }
