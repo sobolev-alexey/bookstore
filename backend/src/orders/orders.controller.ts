8832225e1a00c7ac342d84caae1635214ae7eb98
Alexey Sobolev <sobolev@mail.de>
2022-09-28 19:24:34 +0200

    Order controller

--- backend/src/orders/orders.controller.ts
+++ backend/src/orders/orders.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
+import { CreatePaymentDto } from './dto/create-payment.dto';
 
 @Controller()
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  @Post('order')
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto);
+  @Post('api/order')
+  order(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersService.createOrder(createOrderDto);
+  }
+
+  @Post('api/payment')
+  paymentIntent(@Body() createPaymentDto: CreatePaymentDto) {
+    return this.ordersService.createPaymentIntent(createPaymentDto);
   }
 }
